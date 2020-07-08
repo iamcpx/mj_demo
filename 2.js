214d3781a7b30bac07e6b063e8a0da88f659186f
Mengjing Zhu <mengjing0818@gmail.com>
2020-07-07 23:48:43 -0700

    only 2 changed

--- 2.js
+++ 2.js
@@ -2,7 +2,7 @@
 function waitThreeSeconds() {
     var ms = 5000 + new Date().getTime();
     while (new Date() < ms){}
-    console.log('finished function');
+    console.log('completed function');
 }
 
 function clickHandler() {
